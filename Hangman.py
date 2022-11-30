ffc0fcc7739dcaf07185b75982b423827e111493
1playerone1 <playerone1923@gmail.com>
2022-11-30 23:58:28 +0300

    added wihle loop

--- Hangman.py
+++ Hangman.py
@@ -4,20 +4,26 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-guess = input("Guess a letter: ").lower()
-
+#Create blanks
 display = []
-
 word_lenght = len(chosen_word)
-
 for letter in range(word_lenght):
     display += "_"
 
-for position in range(word_lenght):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    #Check guessed letter
+    for position in range(word_lenght):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You win!!!")
